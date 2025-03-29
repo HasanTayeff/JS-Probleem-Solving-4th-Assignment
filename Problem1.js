208d4b1053e4f41dc5205fd3bc25fee32657a4ba
Hasan Ahmed Tayef <tayeff20@gmail.com>
2025-03-30 01:24:11 +0600

    Corrected

--- Problem1.js
+++ Problem1.js
@@ -1,12 +1,11 @@
 function calculateVAT( price ) {
     if(typeof price === 'number' && price > 0){
         let vat = price * (7.5 / 100);
-        let productPrice = vat + price
-        return productPrice;
+        return vat;
     }
     else {
         const invalidMesssage = 'Invalid'
         return invalidMesssage;
     }
 }
-console.log(calculateVAT('100'))
+console.log(calculateVAT(100))
--- test.js
+++ test.js
@@ -1,15 +1,13 @@
 function calculateVAT( price ) {
     if(typeof price === 'number' && price > 0){
         let vat = price * (7.5 / 100);
-        let productPrice = vat + price;
-        return productPrice;
+        return vat;
     }
     else {
-        const invalidMesssage = 'Invalid';
+        const invalidMesssage = 'Invalid'
         return invalidMesssage;
     }
 }
-
 
 
 
